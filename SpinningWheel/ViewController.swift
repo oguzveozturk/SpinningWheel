11004ca249542372f70a31336a7d0a8dc381c568
oguzveozturk <oguzveozturk@gmail.com>
2020-06-26 20:57:00 +0300

    Initial Commit

---
+++ SpinningWheel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpinningWheel
+//
+//  Created by Oguz on 26.06.2020.
+//  Copyright © 2020 Oguz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
